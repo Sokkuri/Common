1ef1e5cb32a14a2582da66a16f9026551d57dc6f
TeraNovaLP <TeraNovaLP@protonmail.com>
2021-11-12 22:15:31 +0100

    Rebuild constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,172 +2,145 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// #region Languages
+// #region Builder
 
-const applicationLanguagePrefix = "global.language.";
+function buildKey(entity: "global" | "anime" | "company" | "userListEntry", ...args: string[]) {
+    return `${entity}.${args.join(".")}`;
+}
 
-const german = applicationLanguagePrefix + "german";
-const english = applicationLanguagePrefix + "english";
-const japanese = applicationLanguagePrefix + "japanese";
+function buildSeason(season: string) {
+    return buildKey("global", "season", season);
+}
 
-// #endregion
+function buildTag(tag: string) {
+    return buildKey("global", "tag", tag);
+}
 
-// #region Adaptions
+function buildAnimeType(type: string) {
+    return buildKey("anime", "type", type);
+}
 
-const adaptionPrefix = "global.entry.adaption.";
+function buildAnimeSource(source: string) {
+    return buildKey("anime", "source", source);
+}
 
-const anime = adaptionPrefix + "anime";
-const manga = adaptionPrefix + "manga";
-const visualNovel = adaptionPrefix + "visualNovel";
-const lightNovel = adaptionPrefix + "lightNovel";
-const original = adaptionPrefix + "original";
-const game = adaptionPrefix + "game";
-const other = adaptionPrefix + "other";
+function buildAnimeState(state: string) {
+    return buildKey("anime", "state", state);
+}
 
-// #endregion
+function buildCompanyType(type: string) {
+    return buildKey("company", "type", type);
+}
 
-// #region EntryStates
-
-const entryStatePrefix = "global.entry.state.";
-
-const preAiring = entryStatePrefix + "preAiring";
-const airing = entryStatePrefix + "airing";
-const finished = entryStatePrefix + "finished";
-const uncompleted = entryStatePrefix + "uncompleted";
-
-// #endregion
-
-// #region WatchingStates
-
-const watchingStatePrefix = "global.userListEntry.state.";
-
-const planned = watchingStatePrefix + "planned";
-const watching = watchingStatePrefix + "watching";
-const completed = watchingStatePrefix + "completed";
-const paused = watchingStatePrefix + "paused";
-const canceled = watchingStatePrefix + "canceled";
-
-// #endregion
-
-// #region AnimeTypes
-
-const animeTypePrefix = "global.anime.type.";
-
-const series = animeTypePrefix + "series";
-const movie = animeTypePrefix + "movie";
-const special = animeTypePrefix + "special";
-const ova = animeTypePrefix + "ova";
-const ona = animeTypePrefix + "ona";
-
-// #endregion
-
-// #region ExternalLinkTypes
-
-const externalLinkTypePrefix = "global.externalLink.type.";
-
-const video = externalLinkTypePrefix + "video";
-const stream = externalLinkTypePrefix + "stream";
-
-// #endregion
-
-// #region CompanyTypes
-
-const companyTypePrefix = "global.company.type.";
-
-const publisher = companyTypePrefix + "publisher";
-const studio = companyTypePrefix + "studio";
-const streamingService = companyTypePrefix + "streamingService";
-const developerStudio = companyTypePrefix + "developerStudio";
-const tvSender = companyTypePrefix + "tvSender";
-
-// #endregion
-
-// #region Seasons
-
-const seasonPrefix = "global.season.";
-
-const winter = seasonPrefix + "winter";
-const spring = seasonPrefix + "spring";
-const summer = seasonPrefix + "summer";
-const fall = seasonPrefix + "fall";
-
-// #endregion
-
-// #region
-
-const entryTitleTypePrefix = "global.entryTitle.type.";
-
-const title = entryTitleTypePrefix + "title";
-const synonym = entryTitleTypePrefix + "synonym";
+function buildUserListEntryState(state: string) {
+    return buildKey("userListEntry", "state",  state);
+}
 
 // #endregion
 
 export default {
-    Languages: {
-        German: german,
-        English: english,
-        Japanese: japanese
-    },
-    EntryTypes: {
-        Anime: "Anime"
-    },
-    UserList: {
-        EntryState: {
-            Planned: planned,
-            Watching: watching,
-            Completed: completed,
-            Paused: paused,
-            Canceled: canceled,
-            AnimeStates: [ planned, watching, completed, paused, canceled ]
+    Global: {
+        Season: {
+            Winter: buildSeason("winter"),
+            Spring: buildSeason("spring"),
+            Summer: buildSeason("summer"),
+            Fall: buildSeason("fall")
+        },
+        Tag: {
+            Action: buildTag("action"),
+            Adventure: buildTag("adventure"),
+            Alien: buildTag("alien"),
+            Android: buildTag("android"),
+            BoysLove: buildTag("boysLove"),
+            Car: buildTag("car"),
+            Comedy: buildTag("comedy"),
+            Demon: buildTag("demon"),
+            Drama: buildTag("drama"),
+            Ecchi: buildTag("ecchi"),
+            Fantasy: buildTag("fantasy"),
+            Game: buildTag("game"),
+            Ghost: buildTag("ghost"),
+            GirlsLove: buildTag("girlsLove"),
+            Gourmet: buildTag("gourmet"),
+            Harem: buildTag("harem"),
+            Historical: buildTag("historical"),
+            Horror: buildTag("horror"),
+            Idol: buildTag("idol"),
+            Isekai: buildTag("isekai"),
+            Josei: buildTag("josei"),
+            Magic: buildTag("magic"),
+            MagicalGirl: buildTag("magicalGirl"),
+            MartialArts: buildTag("martialArts"),
+            Mecha: buildTag("mecha"),
+            Military: buildTag("military"),
+            Monster: buildTag("monster"),
+            Music: buildTag("music"),
+            Mystery: buildTag("mystery"),
+            Parody: buildTag("parody"),
+            Pirat: buildTag("pirat"),
+            Police: buildTag("police"),
+            Psychological: buildTag("psychological"),
+            Romance: buildTag("romance"),
+            Samurai: buildTag("samurai"),
+            School: buildTag("school"),
+            SciFi: buildTag("sciFi"),
+            Seinen: buildTag("seinen"),
+            Shoujo: buildTag("shoujo"),
+            Shounen: buildTag("shounen"),
+            SliceOfLife: buildTag("sliceOfLife"),
+            Space: buildTag("space"),
+            Sport: buildTag("sport"),
+            Steampunk: buildTag("steampunk"),
+            SuperPower: buildTag("superPower"),
+            Supernatural: buildTag("supernatural"),
+            Thriller: buildTag("thriller"),
+            Tsundere: buildTag("tsundere"),
+            Vampire: buildTag("vampire"),
+            War: buildTag("war"),
+            Witch: buildTag("witch"),
+            Wizard: buildTag("wizard"),
+            Yandere: buildTag("yandere")
         }
     },
-    AnimeTypes: {
-        Series: series,
-        Movie: movie,
-        Special: special,
-        OVA: ova,
-        ONA: ona,
-        AnimeTypes: [ series, movie, special, ova, ona ]
+    Anime: {
+        Type: {
+            Series: buildAnimeType("series"),
+            Movie: buildAnimeType("movie"),
+            Special: buildAnimeType("special"),
+            OVA: buildAnimeType("ova"),
+            ONA: buildAnimeType("ona")
+        },
+        Source: {
+            Manga: buildAnimeSource("manga"),
+            VisualNovel: buildAnimeSource("visualNovel"),
+            LightNovel: buildAnimeSource("lightNovel"),
+            Original: buildAnimeSource("original"),
+            Game: buildAnimeSource("game"),
+            Other: buildAnimeSource("other")
+        },
+        State: {
+            PreAiring: buildAnimeState("preAiring"),
+            Airing: buildAnimeState("airing"),
+            Finished: buildAnimeState("finished"),
+            Uncompleted: buildAnimeState("uncompleted")
+        }
     },
-    ExternalLinkTypes: {
-        Video: video,
-        Stream: stream,
-        ExternalLinkTypes: [ video, stream ]
+    Company: {
+        Type: {
+            Publisher: buildCompanyType("publisher"),
+            Studio: buildCompanyType("studio"),
+            StreamingService: buildCompanyType("streamingService"),
+            DeveloperStudio: buildCompanyType("developerStudio"),
+            TvSender: buildCompanyType("tvSender")
+        }
     },
-    CompanyTypes: {
-        Publisher: publisher,
-        Studio: studio,
-        StreamingService: streamingService,
-        DeveloperStudio: developerStudio,
-        TvSender: tvSender,
-        CompanyTypes: [ publisher, studio, streamingService, developerStudio, tvSender ]
-    },
-    Adaptions: {
-        Anime: anime,
-        Manga: manga,
-        VisualNovel: visualNovel,
-        LightNovel: lightNovel,
-        Original: original,
-        Game: game,
-        Other: other,
-        Adaptions: [ anime, manga, visualNovel, lightNovel, original, game, other ]
-    },
-    EntryStates: {
-        PreAiring: preAiring,
-        Airing: airing,
-        Finished: finished,
-        Uncompleted: uncompleted,
-        States: [ preAiring, airing, finished, uncompleted ]
-    },
-    Seasons: {
-        Winter: winter,
-        Spring: spring,
-        Summer: summer,
-        Fall: fall,
-        Seasons: [ winter, spring, summer, fall ]
-    },
-    EntryTitleTypes: {
-        Title: title,
-        Synonym: synonym
+    UserListEntry: {
+        State: {
+            Planned: buildUserListEntryState("planned"),
+            InProgress: buildUserListEntryState("inProgress"),
+            Completed: buildUserListEntryState("completed"),
+            Paused: buildUserListEntryState("paused"),
+            Canceled: buildUserListEntryState("canceled")
+        }
     }
 };
